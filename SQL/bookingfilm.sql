f8e47e305ea9883c3e88885e86969292ef43cd48
Ngdang1512 <3123411072@sv.sgu.edu.vn>
2025-11-05 13:19:30 +0700

    Update bookingfilm.sql

--- SQL/bookingfilm.sql
+++ SQL/bookingfilm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 04, 2025 lúc 05:14 PM
+-- Thời gian đã tạo: Th10 05, 2025 lúc 07:18 AM
 -- Phiên bản máy phục vụ: 10.4.32-MariaDB
 -- Phiên bản PHP: 8.2.12
 
@@ -41,7 +41,13 @@ CREATE TABLE `chitietdatve` (
 INSERT INTO `chitietdatve` (`Id`, `GiaVe`, `IdDonDatVe`, `IdGhe`) VALUES
 (1, 120000, 1, 1),
 (2, 120000, 1, 2),
-(3, 110000, 2, 6);
+(3, 110000, 2, 6),
+(4, 120000, 3, 123),
+(5, 120000, 3, 124),
+(6, 120000, 4, 125),
+(7, 120000, 4, 126),
+(8, 120000, 5, 91),
+(9, 120000, 5, 100);
 
 -- --------------------------------------------------------
 
@@ -64,7 +70,10 @@ CREATE TABLE `dondatve` (
 
 INSERT INTO `dondatve` (`Id`, `NgayDat`, `TongTien`, `TrangThai`, `IdNguoiDung`, `IdSuatChieu`) VALUES
 (1, '2025-10-30 22:41:18', 240000, 'DaThanhToan', 1, 1),
-(2, '2025-10-30 22:41:18', 110000, 'DaThanhToan', 1, 3);
+(2, '2025-10-30 22:41:18', 110000, 'DaThanhToan', 1, 3),
+(3, '2025-11-05 01:11:46', 240000, 'DaThanhToan', 3, 1),
+(4, '2025-11-05 01:13:43', 240000, 'DaThanhToan', 3, 1),
+(5, '2025-11-05 02:59:07', 240000, 'DaThanhToan', 4, 1);
 
 -- --------------------------------------------------------
 
@@ -84,16 +93,195 @@ CREATE TABLE `ghe` (
 --
 
 INSERT INTO `ghe` (`Id`, `TenGhe`, `LoaiGhe`, `IdPhongChieu`) VALUES
-(1, 'A1', 'Thuong', 1),
-(2, 'A2', 'Thuong', 1),
-(3, 'A3', 'Thuong', 1),
-(4, 'B1', 'VIP', 1),
-(5, 'B2', 'VIP', 1),
 (6, 'C1', 'Thuong', 3),
 (7, 'C2', 'Thuong', 3),
 (8, 'C3', 'Thuong', 3),
 (9, 'D1', 'VIP', 3),
-(10, 'D2', 'VIP', 3);
+(10, 'D2', 'VIP', 3),
+(91, 'A1', 'Thuong', 1),
+(92, 'A2', 'Thuong', 1),
+(93, 'A3', 'Thuong', 1),
+(94, 'A4', 'Thuong', 1),
+(95, 'A5', 'Thuong', 1),
+(96, 'A6', 'Thuong', 1),
+(97, 'A7', 'Thuong', 1),
+(98, 'A8', 'Thuong', 1),
+(99, 'A9', 'Thuong', 1),
+(100, 'A10', 'Thuong', 1),
+(101, 'B1', 'Thuong', 1),
+(102, 'B2', 'Thuong', 1),
+(103, 'B3', 'Thuong', 1),
+(104, 'B4', 'Thuong', 1),
+(105, 'B5', 'Thuong', 1),
+(106, 'B6', 'Thuong', 1),
+(107, 'B7', 'Thuong', 1),
+(108, 'B8', 'Thuong', 1),
+(109, 'B9', 'Thuong', 1),
+(110, 'B10', 'Thuong', 1),
+(111, 'C1', 'Thuong', 1),
+(112, 'C2', 'Thuong', 1),
+(113, 'C3', 'Thuong', 1),
+(114, 'C4', 'Thuong', 1),
+(115, 'C5', 'Thuong', 1),
+(116, 'C6', 'Thuong', 1),
+(117, 'C7', 'Thuong', 1),
+(118, 'C8', 'Thuong', 1),
+(119, 'C9', 'Thuong', 1),
+(120, 'C10', 'Thuong', 1),
+(121, 'D1', 'Thuong', 1),
+(122, 'D2', 'Thuong', 1),
+(123, 'D3', 'Thuong', 1),
+(124, 'D4', 'Thuong', 1),
+(125, 'D5', 'Thuong', 1),
+(126, 'D6', 'Thuong', 1),
+(127, 'D7', 'Thuong', 1),
+(128, 'D8', 'Thuong', 1),
+(129, 'D9', 'Thuong', 1),
+(130, 'D10', 'Thuong', 1),
+(131, 'E1', 'Thuong', 1),
+(132, 'E2', 'Thuong', 1),
+(133, 'E3', 'Thuong', 1),
+(134, 'E4', 'Thuong', 1),
+(135, 'E5', 'Thuong', 1),
+(136, 'E6', 'Thuong', 1),
+(137, 'E7', 'Thuong', 1),
+(138, 'E8', 'Thuong', 1),
+(139, 'E9', 'Thuong', 1),
+(140, 'E10', 'Thuong', 1),
+(141, 'F1', 'Thuong', 1),
+(142, 'F2', 'Thuong', 1),
+(143, 'F3', 'Thuong', 1),
+(144, 'F4', 'Thuong', 1),
+(145, 'F5', 'Thuong', 1),
+(146, 'F6', 'Thuong', 1),
+(147, 'F7', 'Thuong', 1),
+(148, 'F8', 'Thuong', 1),
+(149, 'F9', 'Thuong', 1),
+(150, 'F10', 'Thuong', 1),
+(151, 'G1', 'VIP', 1),
+(152, 'G2', 'VIP', 1),
+(153, 'G3', 'VIP', 1),
+(154, 'G4', 'VIP', 1),
+(155, 'G5', 'VIP', 1),
+(156, 'G6', 'VIP', 1),
+(157, 'G7', 'VIP', 1),
+(158, 'G8', 'VIP', 1),
+(159, 'G9', 'VIP', 1),
+(160, 'G10', 'VIP', 1),
+(161, 'H1', 'VIP', 1),
+(162, 'H2', 'VIP', 1),
+(163, 'H3', 'VIP', 1),
+(164, 'H4', 'VIP', 1),
+(165, 'H5', 'VIP', 1),
+(166, 'H6', 'VIP', 1),
+(167, 'H7', 'VIP', 1),
+(168, 'H8', 'VIP', 1),
+(169, 'H9', 'VIP', 1),
+(170, 'H10', 'VIP', 1),
+(171, 'I1', 'VIP', 4),
+(172, 'I2, I3, I4', 'VIP', 4),
+(173, 'I1', 'VIP', 1),
+(174, 'I2', 'VIP', 1),
+(175, 'A1', 'Thuong', 8),
+(176, 'A2', 'Thuong', 8),
+(177, 'A3', 'Thuong', 8),
+(178, 'A4', 'Thuong', 8),
+(179, 'A5', 'Thuong', 8),
+(180, 'A6', 'Thuong', 8),
+(181, 'A7', 'Thuong', 8),
+(182, 'A8', 'Thuong', 8),
+(183, 'A9', 'Thuong', 8),
+(184, 'A10', 'Thuong', 8),
+(185, 'B1', 'Thuong', 8),
+(186, 'B2', 'Thuong', 8),
+(187, 'B3', 'Thuong', 8),
+(188, 'B4', 'Thuong', 8),
+(189, 'B5', 'Thuong', 8),
+(190, 'B6', 'Thuong', 8),
+(191, 'B7', 'Thuong', 8),
+(192, 'B8', 'Thuong', 8),
+(193, 'B9', 'Thuong', 8),
+(194, 'B10', 'Thuong', 8),
+(195, 'C1', 'Thuong', 8),
+(196, 'C2', 'Thuong', 8),
+(197, 'C3', 'Thuong', 8),
+(198, 'C4', 'Thuong', 8),
+(199, 'C5', 'Thuong', 8),
+(200, 'C6', 'Thuong', 8),
+(201, 'C7', 'Thuong', 8),
+(202, 'C8', 'Thuong', 8),
+(203, 'C9', 'Thuong', 8),
+(204, 'C10', 'Thuong', 8),
+(205, 'D1', 'Thuong', 8),
+(206, 'D2', 'Thuong', 8),
+(207, 'D3', 'Thuong', 8),
+(208, 'D4', 'Thuong', 8),
+(209, 'D5', 'Thuong', 8),
+(210, 'D6', 'Thuong', 8),
+(211, 'D7', 'Thuong', 8),
+(212, 'D8', 'Thuong', 8),
+(213, 'D9', 'Thuong', 8),
+(214, 'D10', 'Thuong', 8),
+(215, 'E1', 'Thuong', 8),
+(216, 'E2', 'Thuong', 8),
+(217, 'E3', 'Thuong', 8),
+(218, 'E4', 'Thuong', 8),
+(219, 'E5', 'Thuong', 8),
+(220, 'E6', 'Thuong', 8),
+(221, 'E7', 'Thuong', 8),
+(222, 'E8', 'Thuong', 8),
+(223, 'E9', 'Thuong', 8),
+(224, 'E10', 'Thuong', 8),
+(225, 'F1', 'Thuong', 8),
+(226, 'F2', 'Thuong', 8),
+(227, 'F3', 'Thuong', 8),
+(228, 'F4', 'Thuong', 8),
+(229, 'F5', 'Thuong', 8),
+(230, 'F6', 'Thuong', 8),
+(231, 'F7', 'Thuong', 8),
+(232, 'F8', 'Thuong', 8),
+(233, 'F9', 'Thuong', 8),
+(234, 'F10', 'Thuong', 8),
+(235, 'G1', 'VIP', 8),
+(236, 'G2', 'VIP', 8),
+(237, 'G3', 'VIP', 8),
+(238, 'G4', 'VIP', 8),
+(239, 'G5', 'VIP', 8),
+(240, 'G6', 'VIP', 8),
+(241, 'G7', 'VIP', 8),
+(242, 'G8', 'VIP', 8),
+(243, 'G9', 'VIP', 8),
+(244, 'G10', 'VIP', 8),
+(245, 'H1', 'VIP', 8),
+(246, 'H2', 'VIP', 8),
+(247, 'H3', 'VIP', 8),
+(248, 'H4', 'VIP', 8),
+(249, 'H5', 'VIP', 8),
+(250, 'H6', 'VIP', 8),
+(251, 'H7', 'VIP', 8),
+(252, 'H8', 'VIP', 8),
+(253, 'H9', 'VIP', 8),
+(254, 'H10', 'VIP', 8),
+(255, 'I1', 'VIP', 8),
+(256, 'I2', 'VIP', 8),
+(257, 'I3', 'VIP', 8),
+(258, 'I4', 'VIP', 8),
+(259, 'I5', 'VIP', 8),
+(260, 'I6', 'VIP', 8),
+(261, 'I7', 'VIP', 8),
+(262, 'I8', 'VIP', 8),
+(263, 'I9', 'VIP', 8),
+(264, 'I10', 'VIP', 8),
+(265, 'J1', 'VIP', 8),
+(266, 'J2', 'VIP', 8),
+(267, 'J3', 'VIP', 8),
+(268, 'J4', 'VIP', 8),
+(269, 'J5', 'VIP', 8),
+(270, 'J6', 'VIP', 8),
+(271, 'J7', 'VIP', 8),
+(272, 'J8', 'VIP', 8),
+(273, 'J9', 'VIP', 8),
+(274, 'J10', 'VIP', 8);
 
 -- --------------------------------------------------------
 
@@ -116,7 +304,9 @@ CREATE TABLE `nguoidung` (
 
 INSERT INTO `nguoidung` (`Id`, `HoTen`, `Email`, `MatKhau`, `SoDienThoai`, `LoaiNguoiDung`) VALUES
 (1, 'Khách Hàng A', 'user@gmail.com', '123456', NULL, 'KhachHang'),
-(2, 'Quản Trị Viên', 'admin@gmail.com', 'admin123', NULL, 'QuanTri');
+(2, 'Quản Trị Viên', 'admin@gmail.com', 'admin123', NULL, 'QuanTri'),
+(3, 'Nguyen Dang', 'admin1@gmail.com', '$2y$10$7oXkmIWhE.omoAqF6tlOs.MU60BEwFEzDaRgq0dNsVy48FLKq/ZUW', NULL, 'QuanTri'),
+(4, 'NgDang', 'user1@gmail.com', '$2y$10$k.qxQo8SOKZ5GtsUf0qy2u0jp2U/QpEYZjYywdW4qqhlbrDltbkCG', NULL, 'KhachHang');
 
 -- --------------------------------------------------------
 
@@ -167,7 +357,9 @@ INSERT INTO `phongchieu` (`Id`, `TenPhong`, `SoLuongGhe`, `IdRap`) VALUES
 (1, 'Phòng 1', 100, 1),
 (2, 'Phòng 2', 120, 1),
 (3, 'Phòng 1', 80, 2),
-(4, 'Phòng 2', 90, 2);
+(4, 'Phòng 2', 90, 2),
+(5, 'Phòng 3', 150, 1),
+(8, 'Phòng 4', 100, 1);
 
 -- --------------------------------------------------------
 
@@ -210,10 +402,10 @@ CREATE TABLE `suatchieu` (
 --
 
 INSERT INTO `suatchieu` (`Id`, `NgayChieu`, `GioBatDau`, `DinhDang`, `GiaVe`, `IdPhim`, `IdPhongChieu`) VALUES
-(1, '2025-10-30', '19:30:00', '2D', 120000, 1, 1),
+(1, '2025-11-05', '19:30:00', '2D', 120000, 1, 1),
 (2, '2025-10-30', '20:00:00', '2D', 150000, 1, 2),
 (3, '2025-10-30', '19:45:00', '2D', 110000, 2, 3),
-(4, '2025-10-31', '21:00:00', '2D', 180000, 3, 4);
+(4, '2025-11-05', '21:00:00', '2D', 180000, 1, 8);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -284,25 +476,25 @@ ALTER TABLE `suatchieu`
 -- AUTO_INCREMENT cho bảng `chitietdatve`
 --
 ALTER TABLE `chitietdatve`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT cho bảng `dondatve`
 --
 ALTER TABLE `dondatve`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT cho bảng `ghe`
 --
 ALTER TABLE `ghe`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=275;
 
 --
 -- AUTO_INCREMENT cho bảng `nguoidung`
 --
 ALTER TABLE `nguoidung`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT cho bảng `phim`
@@ -314,7 +506,7 @@ ALTER TABLE `phim`
 -- AUTO_INCREMENT cho bảng `phongchieu`
 --
 ALTER TABLE `phongchieu`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT cho bảng `rap`
